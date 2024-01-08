d4eda5dc110538faab296a4e07e8ae8cbb010867
osmil3 <oscarstnu@protonmail.com>
2024-01-08 11:08:17 -0300

    add character.interfaces

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()
+
+  public characterlist: Character[]= [{
+    name: 'Trunks',
+    power: 10
+  }]
+}
