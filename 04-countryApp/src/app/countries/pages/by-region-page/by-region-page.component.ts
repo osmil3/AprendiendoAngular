31f550c29064b3530c94604bf9dbef30b6ae5d44
osmil3 <oscarstnu@protonmail.com>
2024-02-01 15:08:14 -0300

    add button region-page

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/contries.service';
 
+type Regions = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,6 +12,7 @@ import { CountriesService } from '../../services/contries.service';
 export class ByRegionPageComponent {
 
   public contries: Country[]= [];
+  public regions: Regions[]= ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   constructor(private countriesService: CountriesService) {}
 
